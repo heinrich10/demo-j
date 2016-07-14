42185b188b082696a50bf8d60a8621ec79ddd024
Heinrich Chan <heinrichchan@gmail.com>
2016-07-14 18:56:21 +0800

    last minute addition

--- src/main/java/hello/CalculatorController.java
+++ src/main/java/hello/CalculatorController.java
@@ -2,6 +2,7 @@ package hello;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -11,7 +12,7 @@ public class CalculatorController {
 	@Autowired
 	CalculatorService calculatorService;
 	
-	@RequestMapping("/calculate")
+	@RequestMapping(value="/calculate", method=RequestMethod.PUT)
 	public Integer calculate(@RequestParam("value") String value) {
 		
 		return calculatorService.calculate(value);
